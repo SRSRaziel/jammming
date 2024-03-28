96c24e8b91fdd95ce19ce2ef2032d6a3fec6beeb
Sullivan Raziel <razielcontato@gmail.com>
2024-03-28 10:19:36 -0300

    Organize Folders and Files

---
+++ src/components/App/App.js
@@ -0,0 +1,14 @@
+import React, {useState} from "react";
+import "./App.css";
+
+export default function App(){
+    return(
+        <>
+            <div class="title">jammming</div>
+            <div class="content">
+                <div class="box">Hello</div>
+                <div class="box">Hello</div>
+            </div>
+        </>
+    )
+}
