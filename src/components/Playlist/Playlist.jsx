7658a4b9b78cca81fec57e149336d5c341fc3a99
Sullivan Raziel <razielcontato@gmail.com>
2024-04-03 09:34:23 -0300

    Creation of Playlist

--- src/components/Playlist/Playlist.jsx
+++ src/components/Playlist/Playlist.jsx
@@ -1,6 +1,22 @@
-import React, { useState } from "react";
+import React, { useCallback } from "react";
 import "./Playlist.css"
+import TrackList from '../TrackList/TrackList';
 
-export default function Playlist(){
+export default function Playlist(props){
+
+    const handleNameChange = useCallback((event) => {
+        props.onNameChange(event.target.value);
+    }, [props.onNameChange]);
+
+    return (
+        <div className="playlist">
+            <input onChange={handleNameChange} defaultValue={"New Playlist"} />
+            <TrackList
+            tracks={props.playlistTracks}
+            isRemoval={true}
+            onRemove={props.onRemove}/>
+            <button className="playlist-save" onClick={props.onSave}> Send to Spotify </button>
+        </div>
+    );
 
 }
--- src/components/SearchResults/SearchResults.jsx
+++ src/components/SearchResults/SearchResults.jsx
@@ -5,11 +5,11 @@ import TrackList from "../TrackList/TrackList";
 export default function SearchResults(props){
 
     return(
-        <div className="searchResults">
-            <h2> Results </h2>
-            <hr/>
-            <TrackList tracks={props.searchResults} onAdd={props.onAdd}/>
-        </div>
+        <>
+            <div className="searchResults">
+                <TrackList tracks={props.searchResults} onAdd={props.onAdd}/>
+            </div>
+        </>
     )
     
 }
